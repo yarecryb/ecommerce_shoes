396a1977f640cc885ddb6ad25f7891a260726c18
Tarj Mecwan <Tarjmecwan007@gmail.com>
2024-05-06 17:43:19 -0700

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends,Query
+from pydantic import BaseModel
+from src.api import auth
+import sqlalchemy
+from src import database as db
 router = APIRouter()
 
 
 @router.get("/catalog/", tags=["catalog"])
-def get_catalog():
-    """
-    Each unique item combination must have only a single price.
-    """
-
-    return [
-            {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": 1,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }
-        ]
+def get_catalog(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1)):
+    with db.engine.connect() as connection:
+        # Define the specific column order in the SELECT statement
+        query = sqlalchemy.text("""
+            SELECT id, username, title, brand, size, price, created_at FROM catalog
+            ORDER BY id ASC
+            OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY
+        """)
+        offset = (page - 1) * page_size
+        result = connection.execute(query, {'offset': offset, 'limit': page_size})
+        catalog_items = [row._asdict() for row in result]
+        return catalog_items
